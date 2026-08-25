43d314a9c7e4e0e995bc7fc91ec16d68fabe047c
Ryan <rbrinkl3@charlotte.edu>
2026-08-24 20:00:58 -0400

    Exercises 1 and 2 Drafted

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,17 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+    let rna: string = "";
+    for (const chr of dna) {
+        if (chr == 'A') { 
+            rna += 'U'; 
+        } else if (chr == 'T') {
+            rna += 'A';
+        } else if (chr == 'C') {
+            rna += 'G';
+        } else if (chr == 'G') {
+            rna += 'C';
+        } else {
+            throw new Error("There was an invalid character in the DNA script: " + chr);
+        }
+    }
+  return rna;
 }
