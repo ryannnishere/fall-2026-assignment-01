50c351ed5403e72613858202038c42a34fa67ae5
ryannnishere <rhb111006@gmail.com>
2026-09-02 18:07:16 -0400

    Exercise 12

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,11 @@
+export type RemoteUser = {
+   id: number;
+   name: string;
+   email: string;
+};
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = (await response.json()) as RemoteUser[];
+  return data.map((user) => user.email);
 }
