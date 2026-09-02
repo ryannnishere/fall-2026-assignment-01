8c8af201490ff6927afede635cb8ab0bf45b2007
ryannnishere <rhb111006@gmail.com>
2026-09-02 17:44:40 -0400

    Exercise 10

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,40 @@
-type UserAccount = {};
+type UserAccount = {
+    id: string;
+    createdAt: Date;
+    email: string;
+    passwordHash: string;
+    profile: {
+        bio: string;
+        avatarUrl: string;
+    }
+};
 
 export class UserRegistry {
+  private users: Map<string, UserAccount> = new Map();
+
   public registerUser(
     data: Omit<UserAccount, 'id' | 'createdAt'>,
   ): UserAccount {
-    return {};
+    const newUser: UserAccount = {
+        ...data,
+        id: crypto.randomUUID(),
+        createdAt: new Date(),
+    };
+    this.users.set(newUser.id, newUser);
+    return newUser;
   }
 
   public getUserView(
     id: string,
   ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+    const user = this.users.get(id);
+    if (!user) {
+        return undefined;
+    }
+    return Object.freeze({
+        id: user.id,
+        email: user.email,
+        profile: user.profile,
+    });
   }
 }
