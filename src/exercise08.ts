7238085848ee1cb0508becefb8e419d550e0a355
ryannnishere <rhb111006@gmail.com>
2026-09-02 15:05:53 -0400

    Exercise 8 & Exercise 9

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,23 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private handlers: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if(!this.handlers[eventName]) {
+        this.handlers[eventName] = [];
+    }
+    this.handlers[eventName]!.push(callback);
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    this.handlers[eventName]?.forEach((callback) => {
+        callback(data);
+    });
+  }
 }
